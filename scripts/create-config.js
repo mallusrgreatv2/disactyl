3f3831fa84948594b6f22b38802f633088b61d15
mallusrgreatv2 <mallusrgreat@gmail.com>
2025-03-16 21:57:50 +0530

    feat: reset config

--- scripts/create-config.js
+++ scripts/create-config.js
@@ -1,6 +1,27 @@
 import chalk from "chalk";
 import fs from "fs";
+import rl from "readline/promises";
 if (!fs.existsSync("src/config.ts")) {
+  createConfig();
+} else {
+  const i = rl.createInterface(process.stdin, process.stdout);
+  let str = (
+    await i.question(
+      chalk.red(
+        "The config already exists! Do you want to reset the config? [y/N] "
+      )
+    )
+  ).toLowerCase();
+  while (str && str !== "y" && str !== "n") {
+    str = (
+      await i.question(chalk.red("Pick a valid option! [y/N]"))
+    ).toLowerCase();
+  }
+  if (str === "y") createConfig();
+  else console.log(`Cancelled!`);
+  i.close();
+}
+function createConfig() {
   fs.writeFileSync(
     "src/config.ts",
     fs
@@ -13,6 +34,4 @@ if (!fs.existsSync("src/config.ts")) {
   console.log(
     `${chalk.green("Created config!")} Open ${chalk.yellow("src/config.ts")} and configure the options.\nThen you can start the bot using ${chalk.yellow("npm run start")}.`
   );
-} else {
-  console.log();
 }
